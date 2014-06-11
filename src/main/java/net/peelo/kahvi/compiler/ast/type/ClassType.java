d70119dabc878a6c94cc80bff7b544a419afa83d
cappu <da.cappu@gmail.com>
2014-06-12 02:11:41 +0300

    add type arguments

--- src/main/java/net/peelo/kahvi/compiler/ast/type/ClassType.java
+++ src/main/java/net/peelo/kahvi/compiler/ast/type/ClassType.java
@@ -4,19 +4,47 @@ import net.peelo.kahvi.compiler.ast.AtomVisitor;
 import net.peelo.kahvi.compiler.util.Name;
 import net.peelo.kahvi.compiler.util.SourcePosition;
 
+import java.util.Collections;
+import java.util.List;
+
 public final class ClassType extends ReferenceType
 {
+    /** Qualified name of the type. */
     private final Name className;
+    /** Optional type arguments. */
+    private final List<TypeArgument> typeArguments;
 
     public ClassType(SourcePosition position, Name className)
     {
-        super(position);
-        this.className = className;
+        this(position, className, Collections.<TypeArgument>emptyList());
     }
 
+    public ClassType(SourcePosition position,
+                     Name className,
+                     List<TypeArgument> typeArguments)
+    {
+        super(position);
+        this.className = className;
+        for (TypeArgument ta : (this.typeArguments = typeArguments))
+        {
+            ta.setEnclosingScope(this);
+        }
+    }
+
+    /**
+     * Returns qualified name of the class.
+     */
     public Name getClassName()
     {
         return this.className;
+    }
+
+    /**
+     * Returns list of optional type arguments.
+     */
+    public List<TypeArgument> getTypeArguments()
+    {
+        return this.typeArguments;
     }
 
     @Override
@@ -40,6 +68,27 @@ public final class ClassType extends ReferenceType
     @Override
     public String toString()
     {
-        return this.className.toString();
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(this.className);
+        if (!this.typeArguments.isEmpty())
+        {
+            boolean first = true;
+
+            sb.append('<');
+            for (TypeArgument ta : this.typeArguments)
+            {
+                if (first)
+                {
+                    first = false;
+                } else {
+                    sb.append(", ");
+                }
+                sb.append(ta);
+            }
+            sb.append('>');
+        }
+
+        return sb.toString();
     }
 }
